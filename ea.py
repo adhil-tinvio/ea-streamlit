3bee8c86fd51097b818a07f6f9bb9e8371e9c48e
Adhil E <adhil.e@tinvio.com>
2024-06-06 16:49:30 +0530

    stream lit init

---
+++ ea.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import boto3
+from io import BytesIO
+import time
+
+
+def analyze_expense_async(s3_bucket, s3_file):
+    # Initialize the Textract client
+    textract_client = boto3.client('textract')
+    print(s3_bucket, s3_file, "s#T")
+    try:
+        # Start the asynchronous expense analysis
+        response = textract_client.start_expense_analysis(
+            DocumentLocation={
+                'S3Object': {
+                    'Bucket': s3_bucket,
+                    'Name': s3_file
+                }
+            }
+        )
+
+        job_id = response['JobId']
+        print(f'Started job with ID: {job_id}')
+
+        # Check the status of the job
+        while True:
+            status = textract_client.get_expense_analysis(JobId=job_id)
+            job_status = status['JobStatus']
+
+            if job_status in ['SUCCEEDED', 'FAILED']:
+                break
+
+            print('Job status:', job_status)
+            time.sleep(5)
+
+        if job_status == 'SUCCEEDED':
+            # Get the results of the expense analysis
+            result_pages = []
+            next_token = None
+
+            while True:
+                if next_token:
+                    result = textract_client.get_expense_analysis(JobId=job_id, NextToken=next_token)
+                else:
+                    result = textract_client.get_expense_analysis(JobId=job_id)
+
+                result_pages.append(result)
+                next_token = result.get('NextToken')
+
+                if not next_token:
+                    break
+
+            print('Expense analysis completed successfully.')
+            output_file = s3_file + "_op"
+            # Save the response to a text file
+            with open(output_file, 'w') as f:
+                for page in result_pages:
+                    f.write(str(page) + '\n')
+
+            with open(output_file, 'r') as f:
+                return f.read()
+        else:
+            print('Expense analysis failed.')
+            return None
+
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
+
+
+def main():
+    st.title('Expense Analyzer Textract')
+    ea_file = st.file_uploader("Upload Invoice/Bill", type=['pdf', 'jpg', 'png'])
+    file_name = ea_file.name
+    file_bytes = BytesIO(ea_file.getvalue())
+    s3_client = boto3.client('s3')
+    s3_client.upload_fileobj(file_bytes, st.secrets['BUCKET_NAME'], file_name)
+
+    if ea_file is not None:
+        output = analyze_expense_async(st.secrets['BUCKET_NAME'], ea_file.name)
+        st.download_button(
+            label="Download text file Response",
+            data=output,
+            file_name=file_name + "+response.txt",
+            mime='text/plain',
+        )
